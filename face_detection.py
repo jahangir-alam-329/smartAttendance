42aef43b51250b9e5f5a109b6216cfbbb131bf85
jahangir-alam-329 <sadabrajra8@gmail.com>
2024-09-20 09:09:14 +0530

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -1,39 +1,32 @@
-# importing librarys
-import cv2
-import numpy as npy
-import face_recognition as face_rec
-# function
-def resize(img, size) :
-    width = int(img.shape[1]*size)
-    height = int(img.shape[0] * size)
-    dimension = (width, height)
-    return cv2.resize(img, dimension, interpolation= cv2.INTER_AREA)
+import streamlit as st
+button=st.button("click me")
+if button==False:
+    picture = st.camera_input("Take a picture")
+
+    if picture:
+        p=st.image(picture)
 
 
-# img declaration
-jahangir = face_rec.load_image_file('sampe_images\jahangir.jpg')
-jahangir = cv2.cvtColor(jahangir, cv2.COLOR_BGR2RGB)
-jahangir = resize(jahangir, 0.50)
-jahangir_test = face_rec.load_image_file('sampe_images\jahangir_test4.jpg')
-jahangir_test = resize(jahangir_test, 0.50)
-jahangir_test = cv2.cvtColor(jahangir_test, cv2.COLOR_BGR2RGB)
+# to start live_webcam on streamlit
 
-# finding face location
-
-faceLocation_jahangir = face_rec.face_locations(jahangir)[0]
-encode_jahangir = face_rec.face_encodings(jahangir)[0]
-cv2.rectangle(jahangir, (faceLocation_jahangir[3], faceLocation_jahangir[0]), (faceLocation_jahangir[1], faceLocation_jahangir[2]), (255, 0, 255), 3)
+elif button==True:
+    import cv2
+    from cvzone.FaceDetectionModule import FaceDetector
+    import streamlit as st
 
 
-faceLocation_jahangirtest = face_rec.face_locations(jahangir_test)[0]
-encode_jahangirtest = face_rec.face_encodings(jahangir_test)[0]
-cv2.rectangle(jahangir_test, (faceLocation_jahangir[3], faceLocation_jahangir[0]), (faceLocation_jahangir[1], faceLocation_jahangir[2]), (255, 0, 255), 3)
+    st.title("web app using opencv cvzone and streamlit")
+    st.title("face dection")
+    run=st.checkbox("Run")
+    frame_window=st.image([])
+    detector=FaceDetector()
+    cap=cv2.VideoCapture(0)
+    while True:
+        ret, img=cap.read()
+        imgRGB=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        face, New_Image=detector.findFaces(img)
+        q=frame_window.image(img)
+        
 
-results = face_rec.compare_faces([encode_jahangir], encode_jahangirtest)######
-print(results)
-cv2.putText(jahangir_test, f'{results}', (50,50), cv2.FONT_HERSHEY_COMPLEX, 1,(0,0,255), 2 )
-
-cv2.imshow('main_img', jahangir)
-cv2.imshow('test_img', jahangir_test)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+else:
+    st.success("click button")
